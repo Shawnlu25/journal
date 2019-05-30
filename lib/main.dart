15a1b2f402cb33504c5d6495a4ee49d4cc20b997
Yiyi Wang <shd101wyy@gmail.com>
2019-05-30 15:16:56 +0800

    Add flutter_sticky_header

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,201 @@
 import 'package:flutter/material.dart';
+import 'package:journal/models/record_time.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+import './widgets/activity_overview.dart';
 
-void main() => runApp(MyApp());
+final List<Map<String, dynamic>> fakeData = [
+  {
+    "title": "在办公室工作 1",
+    "startTime": DateTime(2019, 5, 27, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 27, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 2",
+    "startTime": DateTime(2019, 5, 27, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 27, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 3",
+    "startTime": DateTime(2019, 5, 27, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 27, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 4",
+    "startTime": DateTime(2019, 5, 27, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 27, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 5",
+    "startTime": DateTime(2019, 5, 27, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 27, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 6",
+    "startTime": DateTime(2019, 5, 27, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 27, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 7",
+    "startTime": DateTime(2019, 5, 27, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 27, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 8",
+    "startTime": DateTime(2019, 5, 27, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 27, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 9",
+    "startTime": DateTime(2019, 5, 27, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 27, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 1",
+    "startTime": DateTime(2019, 5, 28, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 28, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 2",
+    "startTime": DateTime(2019, 5, 28, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 28, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 3",
+    "startTime": DateTime(2019, 5, 28, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 28, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 4",
+    "startTime": DateTime(2019, 5, 28, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 28, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 5",
+    "startTime": DateTime(2019, 5, 28, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 28, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 6",
+    "startTime": DateTime(2019, 5, 28, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 28, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 7",
+    "startTime": DateTime(2019, 5, 28, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 28, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 8",
+    "startTime": DateTime(2019, 5, 28, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 28, 11, 30).millisecondsSinceEpoch
+  },
+  {
+    "title": "在办公室工作 9",
+    "startTime": DateTime(2019, 5, 28, 11, 0).millisecondsSinceEpoch,
+    "endTime": DateTime(2019, 5, 28, 11, 30).millisecondsSinceEpoch
+  },
+];
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+main() => runApp(MaterialApp(
+      home: MyHomePage(),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        brightness: Brightness.light,
+        //primaryColor: Color.fromRGBO(59, 66, 84, 1),
+        //scaffoldBackgroundColor: Color.fromRGBO(59, 66, 84, 1),
+        //accentColor: Colors.cyan[600],
+        //backgroundColor: Colors.lightBlue[800],
+        //cardTheme: CardTheme(color: Color.fromRGBO(84, 92, 110, 1)),
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
+
+class MyHomePage extends StatelessWidget {
+  List<Widget> _buildOverviewList() {
+    var groups = [];
+    fakeData.forEach((data) {
+      var startTime = DateTime.fromMillisecondsSinceEpoch(data["startTime"]);
+      if (groups.isNotEmpty) {
+        var lastStartTime = DateTime.fromMillisecondsSinceEpoch(
+            groups[groups.length - 1][0]["startTime"]);
+
+        if (startTime.year == lastStartTime.year &&
+            startTime.month == lastStartTime.month &&
+            startTime.day == lastStartTime.day) {
+          groups[groups.length - 1].add(data);
+        } else {
+          groups.add([data]);
+        }
+      } else {
+        groups.add([data]);
+      }
+    });
+
+    return groups.map((records) {
+      return SliverStickyHeaderBuilder(
+          overlapsContent: true,
+          builder: (context, state) {
+            return Container(
+              color: Colors.transparent,
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                        ActivityOverview.WeekdayMap[
+                            DateTime.fromMillisecondsSinceEpoch(
+                                    records[0]["startTime"])
+                                .weekday],
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.w300)),
+                    Text(
+                      //startTime.day.toString(),
+                      DateTime.fromMillisecondsSinceEpoch(
+                              records[0]["startTime"])
+                          .day
+                          .toString(),
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          sliver: new SliverList(
+            delegate: new SliverChildBuilderDelegate(
+              (context, i) {
+                return Padding(
+                  padding: EdgeInsets.only(left: 8, right: 8),
+                  child: ActivityOverview(
+                    title: records[i]["title"],
+                    startTime: DateTime.fromMillisecondsSinceEpoch(
+                        records[i]["startTime"]),
+                    endTime: DateTime.fromMillisecondsSinceEpoch(
+                        records[i]["endTime"]),
+                  ),
+                );
+              },
+              childCount: records.length,
+            ),
+          ));
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("点点时刻しました"),
+        ),
+        body: CustomScrollView(
+          slivers: this._buildOverviewList(),
+        ));
   }
 }
