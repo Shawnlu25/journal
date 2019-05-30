52116c762caf0c584640191c4231c968408f23fc
Zefu Lu <shawnlu25@gmail.com>
2019-05-30 14:13:46 +0800

    Refactored journal_entry and added compareTo method

--- lib/models/journal_entry.dart
+++ lib/models/journal_entry.dart
@@ -1,43 +1,99 @@
 import 'package:uuid/uuid.dart';
-import 'package:journal/models/tag.dart';
+//import 'package:journal/models/tag.dart';
 
 enum JournalEntryState { Past, InProgress, Future }
 
-class JournalEntry {
+class JournalEntry implements Comparable<JournalEntry>{
   String _id;
-  String act;
-  DateTime startTimestamp;
-  DateTime endTimestamp;
-  List<Tag> tags;
-  String description;
+  String _act;
+  DateTime _startTimestamp;
+  DateTime _endTimestamp;
+  //List<Tag> tags;
+  //String description;
 
-  JournalEntry(DateTime startTimestamp) {
+
+  // =========== Constructors ============
+  // Default Constructor
+  JournalEntry(this._id, this._act, this._startTimestamp, this._endTimestamp);
+
+  // Constructor for creating a new entry
+  JournalEntry.createEntry(DateTime startTimestamp) {
     this._id = Uuid().v4();
-    this.act = "";
-    this.description = "";
-    this.startTimestamp = startTimestamp;
-    this.tags = new List<Tag>();
+    this._act = "";
+    this._startTimestamp = startTimestamp;
   }
 
-  String id() {
-    return this._id;
+  // ========== Getter & Setters ==========
+  String get id => _id;
+
+  String get act => _act;
+
+  set act(String value) {
+    _act = value;
   }
+
+  DateTime get startTimestamp => _startTimestamp;
+
+  set startTimestamp(DateTime value) {
+    if (endTimestamp != null && value.isAfter(endTimestamp)) {
+      _endTimestamp = value;
+    }
+    _startTimestamp = value;
+  }
+
+  DateTime get endTimestamp => _endTimestamp;
+
+  set endTimestamp(DateTime value) {
+    if (value.isBefore(startTimestamp)) {
+      _endTimestamp = startTimestamp;
+    } else {
+      _endTimestamp = value;
+    }
+  }
+
+  // ============= Comparable =================
+  @override
+  int compareTo(JournalEntry e) {
+    if (startTimestamp.compareTo(e.startTimestamp) != 0) {
+      return startTimestamp.compareTo(e.startTimestamp);
+    }
+
+    if (endTimestamp != null) {
+      if (e.endTimestamp == null) {
+        return -1;
+      }
+      if (endTimestamp.compareTo(e.endTimestamp) != 0) {
+        return endTimestamp.compareTo(e.endTimestamp);
+      }
+    } else if (e.endTimestamp != null){
+      return 1;
+    }
+
+    if (act.compareTo(e.act) != 0) {
+      return act.compareTo(e.act);
+    }
+
+    return id.compareTo(e.id);
+  }
+
+  // ============= Other methods ==============
 
   JournalEntryState getState() {
     final now = new DateTime.now();
-    if (startTimestamp.isAfter(now)) {
+
+    if (this._startTimestamp.isAfter(now)) {
       return JournalEntryState.Future;
     }
-    if (endTimestamp != null && endTimestamp.isBefore(now)) {
+
+    if (this._endTimestamp != null && this._endTimestamp.isBefore(now)) {
       return JournalEntryState.Past;
     }
+
     return JournalEntryState.InProgress;
   }
 
   Duration getDuration() {
-    if (endTimestamp != null) {
-      return endTimestamp.difference(startTimestamp);
-    }
-    return null;
+    return this._endTimestamp?.difference(this._startTimestamp);
   }
+
 }
--- test/journal_entry_test.dart
+++ test/journal_entry_test.dart
@@ -10,15 +10,15 @@ final tenMinutesAfterCurrent = currentTime.add(new Duration(minutes: 10));
 void main() {
   group("Test id uniqueness", () {
     test("Two different journal entries should have different ids", () {
-      JournalEntry e1 = new JournalEntry(twentyMinutesBeforeCurrent);
-      JournalEntry e2 = new JournalEntry(twentyMinutesBeforeCurrent);
-      expect(e1.id() == e2.id(), false);
+      JournalEntry e1 = JournalEntry.createEntry(twentyMinutesBeforeCurrent);
+      JournalEntry e2 = JournalEntry.createEntry(twentyMinutesBeforeCurrent);
+      expect(e1.id == e2.id, false);
     });
   });
 
   group("Test journal entry state", () {
     test('New journal entry with startTimestamp before current time should have state InProgress', () {
-      JournalEntry e = new JournalEntry(twentyMinutesBeforeCurrent);
+      JournalEntry e = JournalEntry.createEntry(twentyMinutesBeforeCurrent);
       expect(e.getState(), JournalEntryState.InProgress);
     });
   });
