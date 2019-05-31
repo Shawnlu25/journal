11aef774b0f2d8d58b062b15acc012458ecd4647
Zefu Lu <shawnlu25@gmail.com>
2019-05-31 09:57:38 +0800

    implemented Tiled Dashboard widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:journal/tiled_dashboard.dart';
 
-void main(){
+void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.white,
     systemNavigationBarIconBrightness: Brightness.dark,
@@ -17,18 +18,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        bottomSheet: Container(
+          color: Colors.white,
+          //elevation: 8.0,
+          child: TiledDashboard(),
+        ),
+      ),
     );
   }
 }
@@ -52,7 +50,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -61,77 +58,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return MaterialApp(home: HeroAnimation());
-  }
-}
-
-class TextHero extends StatelessWidget {
-  final String text;
-  final VoidCallback onTap;
-  final double fontSize;
-  final String id;
-
-  const TextHero({ Key key, this.id, this.text, this.onTap, this.fontSize }) : super(key: key);
-
-  Widget build(BuildContext context) {
-    return Container(
-      child: Hero(
-        tag: id,
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: onTap,
-            child: Text(
-              text,
-              style: TextStyle(fontSize: fontSize),
-            ),
-          ),
-        ),
-      )
-    );
-  }
-}
-
-class HeroAnimation extends StatelessWidget {
-  Widget build(BuildContext context) {
-    //timeDilation = 5.0; // 1.0 means normal animation speed.
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Basic Hero Animation'),
-      ),
-      body: Center(
-        child: TextHero(
-          id: "test id",
-          text: 'Test',
-          fontSize: 18,
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute<void>(
-                builder: (BuildContext context) {
-                  return Scaffold(
-                    appBar: AppBar(
-                      title: const Text('Test'),
-                    ),
-                    body: Container(
-                      // The blue background emphasizes that it's a new route.
-                      color: Colors.lightBlueAccent,
-                      padding: const EdgeInsets.all(16.0),
-                      alignment: Alignment.topLeft,
-                      child: TextHero(
-                        id: "test id",
-                        text: 'Test...',
-                        fontSize: 30,
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ),
-                  );
-                }
-            ));
-          },
-        ),
-      ),
-    );
+    return MaterialApp(home: Container());
   }
 }
---
+++ lib/tiled_dashboard.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+
+final double tileUnitSize = 48;
+
+class TiledDashboard extends StatefulWidget {
+  @override
+  _TiledDashboardState createState() => _TiledDashboardState();
+}
+
+class _TiledDashboardState extends State<TiledDashboard> {
+  double _height = tileUnitSize * 8;
+
+  @override
+  Widget build(BuildContext context) {
+    // Fill this out in the next steps
+    return AnimatedContainer(
+      height: _height,
+      duration: Duration(milliseconds: 100),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Column(
+          children: [
+            Row(
+              children: <Widget>[
+                TiledTextButton(
+                  onTap: () => {
+                        setState(() {
+                          _height = tileUnitSize * 6;
+                        })
+                      },
+                  flex: 7,
+                  text: "Take an action...",
+                  textStyle: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[500],
+                  ),
+                ),
+                TiledIconButton(
+                  onTap: () => {},
+                  iconData: Icons.more_vert,
+                  flex: 1,
+                  iconSizeFactor: 1.0,
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                TiledTextButton(
+                  onTap: () => {
+                        setState(() {
+                          _height = tileUnitSize * 8;
+                        })
+                      },
+                  flex: 5,
+                  text: "Take an action...",
+                  textStyle: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[500],
+                  ),
+                ),
+                TiledTimeButton(),
+              ],
+            ),
+            TiledTextfield(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TiledTextButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final int flex;
+  final String text;
+  final TextStyle textStyle;
+
+  const TiledTextButton(
+      {Key key, this.onTap, this.flex, this.text, this.textStyle})
+      : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: flex,
+      child: Container(
+        height: tileUnitSize,
+        color: Colors.white,
+        padding: EdgeInsets.all(0.0),
+        child: Material(
+          elevation: 0.0,
+          color: Colors.white,
+          animationDuration: Duration(milliseconds: 100),
+          child: InkWell(
+            onTap: onTap,
+            child: Align(
+              alignment: FractionalOffset(0.1, 0.5),
+              child: Text(
+                text,
+                style: textStyle,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TiledIconButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final IconData iconData;
+  final int flex;
+  final double iconSizeFactor;
+
+  const TiledIconButton(
+      {Key key, this.onTap, this.iconData, this.flex, this.iconSizeFactor})
+      : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: flex,
+      child: Container(
+        height: tileUnitSize,
+        color: Colors.white,
+        padding: EdgeInsets.all(0.0),
+        child: Material(
+          elevation: 0.0,
+          color: Colors.white,
+          animationDuration: Duration(milliseconds: 100),
+          child: InkWell(
+            onTap: onTap,
+            child: Center(
+              child: Icon(
+                iconData,
+                color: Colors.grey[600],
+                size: tileUnitSize * iconSizeFactor / 2.0,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TiledTimeButton extends StatefulWidget {
+  @override
+  _TiledTimeButtonState createState() => _TiledTimeButtonState();
+}
+
+class _TiledTimeButtonState extends State<TiledTimeButton> {
+  DateTime _timestamp = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 3,
+      child: Container(
+        height: tileUnitSize,
+        color: Colors.white,
+        padding: EdgeInsets.all(0.0),
+        child: Material(
+          elevation: 0.0,
+          color: Colors.white,
+          animationDuration: Duration(milliseconds: 100),
+          child: InkWell(
+            onTap: () => {},
+            child: Center(
+                child: Text(
+              "${_timestamp.month} / ${_timestamp.day}  ${_timestamp.hour} : ${_timestamp.minute}",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[600],
+              ),
+            )),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TiledTextfield extends StatelessWidget {
+  const TiledTextfield({Key key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 8,
+      child: Container(
+        height: tileUnitSize * 3,
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: EditableText(
+          controller: TextEditingController(),
+          focusNode: FocusNode(),
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey[600],
+          ),
+          cursorColor: Colors.grey[400],
+          backgroundCursorColor: Colors.grey[100],
+          maxLines: 3,
+        ),
+      ),
+    );
+  }
+}
